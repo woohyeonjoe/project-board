8fab7179ae8a5a95e8685912723cbfad02b71519
woohyeonjoe <qlqkthd8802@gmail.com>
2023-03-22 18:03:59 +0900

    #21 - 댓글 서비스 로직의 테스트와 골격 잡기
    댓글과 관련된 단순 crud 기능을 테스트로 표현
    강의 시간 상 기능의 일부만 테스트로 표현함
    나머지 필요 기능은 별도 작성하여 넣기로.

---
+++ src/main/java/com/example/projectboard/dto/ArticleCommentDto.java
@@ -0,0 +1,10 @@
+package com.example.projectboard.dto;
+
+import java.time.LocalDateTime;
+
+public record ArticleCommentDto(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+
+    public static ArticleCommentDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String content) {
+        return new ArticleCommentDto(createdAt, createdBy, modifiedAt, modifiedBy, content);
+    }
+}
---
+++ src/test/java/com/example/projectboard/service/ArticleCommentServiceTest.java
@@ -0,0 +1,67 @@
+package com.example.projectboard.service;
+
+import com.example.projectboard.domain.Article;
+import com.example.projectboard.domain.ArticleComment;
+import com.example.projectboard.domain.UserAccount;
+import com.example.projectboard.dto.ArticleCommentDto;
+import com.example.projectboard.repository.ArticleCommentRepository;
+import com.example.projectboard.repository.ArticleRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+@DisplayName("비즈니스 로직 - 댓글")
+@ExtendWith(MockitoExtension.class)
+class ArticleCommentServiceTest {
+
+    @InjectMocks private ArticleCommentService sut;
+
+    @Mock private ArticleRepository articleRepository;
+    @Mock private ArticleCommentRepository articleCommentRepository;
+
+    @DisplayName("게시글 ID로 조회하면, 해당하는 댓글 리스트를 반환한다.")
+    @Test
+    void givenArticleId_whenSearchingComments_thenReturnsArticleComment() {
+        //given
+        Long articleId = 1L;
+
+        given(articleRepository.findById(articleId)).willReturn(Optional.of(
+                Article.of(null, "title", "content", "#java")
+        ));
+
+        //when
+        List<ArticleCommentDto> articleComments = sut.searchArticleComment(articleId);
+
+        //then
+        assertThat(articleComments).isNotNull();
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("댓글 정보를 입력하면, 댓글을 저장한다.")
+    @Test
+    void givenArticleCommentInfo_whenSavingArticleComment_thenSavesArticleComment() {
+        // Given
+        given(articleCommentRepository.save(any(ArticleComment.class))).willReturn(null);
+
+        // When
+        sut.saveArticleComment(ArticleCommentDto.of(LocalDateTime.now(), "Uno", LocalDateTime.now(), "Uno", "comment"));
+
+        // Then
+        then(articleCommentRepository).should().save(any(ArticleComment.class));
+    }
+}
