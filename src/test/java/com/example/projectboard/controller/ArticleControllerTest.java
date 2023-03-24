9347ac993eb4ea57bbeba40aad2e4368a66e8fef
woohyeonjoe <qlqkthd8802@gmail.com>
2023-03-24 20:45:44 +0900

    #21 - 게시판 컨트롤러 구현
    게시판 기본 조회 화면을 위한 컨트롤러 구현하고
    데이터 전달 완료
    게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/example/projectboard/controller/ArticleControllerTest.java
@@ -1,17 +1,31 @@
 package com.example.projectboard.controller;
 
 import com.example.projectboard.config.SecurityConfig;
+import com.example.projectboard.dto.ArticleWithCommentsDto;
+import com.example.projectboard.dto.UserAccountDto;
+import com.example.projectboard.service.ArticleService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.time.LocalDateTime;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -22,6 +36,8 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockBean private ArticleService articleService;
+
     ArticleControllerTest(@Autowired MockMvc mvc) {     //원래 생성자가 하나면 @Autowired 가 자동을 붙어지지만 test 패키지에서는 아니다.
         this.mvc = mvc;
     }
@@ -30,6 +46,7 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
         //given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());      //필드 중 일부만 ArgumentMatcher를 사용 할 수 없다. null 값들도 ArgumentMatcher.eq()로 감싸주자.
 
         //when & then
         mvc.perform(get("/articles"))
@@ -37,6 +54,8 @@ class ArticleControllerTest {
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))      //html 형식(하위 형식들 포함) 확인(html/charset-utf8)
                 .andExpect(view().name("articles/index"))   //뷰 이름 검사
                 .andExpect(model().attributeExists("articles"));    //model.addAttribute()로 model에 articles 데이터가 담겨있는지 확인
+
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
 
@@ -44,14 +63,18 @@ class ArticleControllerTest {
     @Test
     public void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception {
         //given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         //when & then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/" + articleId))
                 .andExpect(status().isOk())                                 //200OK 확인
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))      //html 형식 확인
                 .andExpect(view().name("articles/detail"))   //뷰 이름 검사
                 .andExpect(model().attributeExists("article"))    //model.addAttribute()로 model에 article 데이터가 담겨있는지 확인
                 .andExpect(model().attributeExists("articleComments"));    //model.addAttribute()로 model에 articleComments 데이터가 담겨있는지 확인
+
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled("구현 중")
@@ -78,5 +101,34 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())                                 //200OK 확인
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))      //html 형식 확인
                 .andExpect(view().name("articles/search-hashtag"));   //뷰 이름 검사
+    }
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+                1L,
+                createUserAccountDto(),
+                Set.of(),
+                "title",
+                "content",
+                "#java",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(1L,
+                "uno",
+                "pw",
+                "uno@mail.com",
+                "Uno",
+                "memo",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
     }
 }
